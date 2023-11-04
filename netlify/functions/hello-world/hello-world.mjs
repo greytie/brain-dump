058a92ff5aa26a5c284d10d4828717378c148752
Grey Tie <ultrafutile@protonmail.com>
2023-11-04 14:56:23 -0500

    Fix issue where hello-world function was not including headers

--- netlify/functions/hello-world/hello-world.mjs
+++ netlify/functions/hello-world/hello-world.mjs
@@ -1,23 +1,22 @@
 const headers = {
 	'Access-Control-Allow-Origin': '*',
 	'Access-Control-Allow-Headers': 'Content-Type',
-	'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+	'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+	'Access-Control-Allow-Credentials': true
   };
 
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-export default async (req, context) =>{
+export default async (req, context) => {
   try {
-    const subject = req.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello? ${subject}!` }),
-	  headers
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+    const subject = req?.queryStringParameters?.name || 'World'
+	return new Response(JSON.stringify({ message: `Hello? ${subject}!`}), {
+		status: 200,
+		headers
+	});
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+	return new Response(error.toString(), {
+		status: 500,
+		headers
+	})
   }	
 }
-
